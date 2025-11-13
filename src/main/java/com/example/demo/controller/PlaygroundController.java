ff883cfc2eb18754133be1de141e5511bbe0d9ad
Jaicle Pando Moriyón <jpandomoriyon@outlook.es>
2025-11-13 11:02:30 +0100

    Minimal optimizations

--- src/main/java/com/example/demo/controller/PlaygroundController.java
+++ src/main/java/com/example/demo/controller/PlaygroundController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.utils.ConfigurationHelper;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
